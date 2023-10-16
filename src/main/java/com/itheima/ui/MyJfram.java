03c4e27e341d5939ba614cd1dab9f872db3e14ef
langnang <chenlangl@look.com>
2023-10-16 15:11:29 +0800

    gitfirstdemo

--- src/main/java/com/itheima/ui/MyJfram.java
+++ src/main/java/com/itheima/ui/MyJfram.java
@@ -7,6 +7,7 @@ public class MyJfram extends JFrame {
 
     public MyJfram(String title) throws HeadlessException {
         super(title);
+        //设置宽高
         super.setSize(500,500);
 
     }
---
+++ src/main/java/com/itheima/ui/TestFram.java
@@ -0,0 +1,9 @@
+package com.itheima.ui;
+
+public class TestFram {
+    public static void main(String[] args) {
+        MyJfram jfram = new MyJfram("第一个窗体");
+        //设置窗体可见
+        jfram.setVisible(true);
+    }
+}
