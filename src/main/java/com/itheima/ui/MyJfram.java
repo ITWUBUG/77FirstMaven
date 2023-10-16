ccba051a86ad8cb138197851642502b9cc477bf6
langnang <chenlangl@look.com>
2023-10-16 15:05:07 +0800

    gitfirstdemo

--- src/main/java/com/itheima/ui/MyJfram.java
+++ src/main/java/com/itheima/ui/MyJfram.java
@@ -1,7 +1,13 @@
 package com.itheima.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyJfram extends JFrame {
 
+    public MyJfram(String title) throws HeadlessException {
+        super(title);
+        super.setSize(500,500);
+
+    }
 }
